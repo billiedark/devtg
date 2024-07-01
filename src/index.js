b344385232feff314979e45e6047ca9836fd5a24
Vlad <hhxx213@gmail.com>
2024-07-01 15:06:00 +0300

    RevolutionEra-Zero2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { useTelegram } from './hooks/useTelegram';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const isDev = true;
+const isDev = false;
 
 const AppWrapper = () => {
     const [userInfo, setUserInfo] = useState(null);
